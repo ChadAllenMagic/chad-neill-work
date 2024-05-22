a55475d4609d56a61783115f3cb080b5554c0eea
Chad Allen <chadallenmagic@gmail.com>
2024-05-22 13:30:07 -0700

    update greeting-three

---
+++ greeting-three.js
@@ -0,0 +1,10 @@
+
+function greet(name1, name2, name3) {
+  console.log("Hello!" + name1);
+  console.log("Bonjour!" + name2);
+  console.log("Ciao!" + name3);
+}
+
+greet("Clotho", "Lachesis", "Atropos");
+
+greet("Larry", "Curly", "Moe");
