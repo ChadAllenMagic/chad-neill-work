477dc5e2e15b510d6a048adbbedf5bbbd219586c
Chad Allen <chadallenmagic@gmail.com>
2024-05-25 10:25:56 -0700

    Added Ringo

--- greeting-three.js
+++ greeting-three.js
@@ -1,10 +1,14 @@
 
-function greet(name1, name2, name3) {
+function greet(name1, name2, name3, name4) {
   console.log("Hello!" + name1);
   console.log("Bonjour!" + name2);
   console.log("Ciao!" + name3);
+  console.log("Oh, hey." + name4);
 }
 
 greet("Clotho", "Lachesis", "Atropos");
 
 greet("Larry", "Curly", "Moe");
+
+greet("john", "Paul", "George", "Ringo"); 
+
