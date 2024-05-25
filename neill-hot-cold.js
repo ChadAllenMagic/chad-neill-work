5110198b0a3a0e97269968febf3ac1e5d8d08575
Chad Allen <chadallenmagic@gmail.com>
2024-05-25 11:48:23 -0700

    else if

---
+++ neill-hot-cold.js
@@ -0,0 +1,10 @@
+const temperature = 100;
+console.log("The temperature is " + temperature);
+
+if (temperature >= 20) {
+  console.log("and it is hot.");
+} else {
+ console.log("It is cold.")   
+}
+
+console.log("Goodbye.");
