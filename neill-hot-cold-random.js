cd7eef527fc1c046d00acc27d3ce08f0cbebae60
Chad Allen <chadallenmagic@gmail.com>
2024-05-25 12:22:12 -0700

    add math random example

---
+++ neill-hot-cold-random.js
@@ -0,0 +1,10 @@
+const temperature = Math.round(135 * Math.random());
+console.log("The temperature is " + temperature);
+
+if (temperature >= 65) {
+  console.log("and it is hot.");
+} else {
+  console.log("It is cold.");
+}
+
+console.log("Goodbye.");
