1b5303f8d6e51313365c985ab7912fdbb5e5e5bb
Chad Allen <chadallenmagic@gmail.com>
2024-05-29 16:44:55 -0700

    counter exercise

---
+++ squares-loop.js
@@ -0,0 +1,9 @@
+let counter = 0;
+while (counter <= 10) {
+    let result = counter * counter;
+    console.log (counter, "squared is", result);
+    counter = counter +1;
+    
+}
+
+console.log("Finish");
