572397fa4b775334d42166e85b8e80f9b90208cb
Chad Allen <chadallenmagic@gmail.com>
2024-05-30 13:04:12 -0700

    Code fix with >

--- pegasus-party.js
+++ pegasus-party.js
@@ -4,16 +4,9 @@ console.log(greeting);
 
 let die1 = Math.ceil(6 * Math.random());
 console.log(die1);
-let die2 = Math.ceil(6 * Math.random());
-if (die1 === 1) {
-  console.log(die2);
-}
-
-let die3 = Math.ceil(6 * Math.random());
-if (die2 === 2) {
-  console.log(die3);
-}
-let greeting2 = "Party on Pegasus!";
-if (die3 === 3) {
-  console.log(greeting2);
+let greeting2 = "Poor Pegasus";
+let greeting3 = "Party on Pegasus!";
+if (die1 > 1) {console.log (greeting2);
+} else {
+  console.log (greeting3);
 }
