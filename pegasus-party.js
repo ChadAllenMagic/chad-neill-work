59c1ad97d667f3c7cb51b62b44f87097b90d4afe
Chad Allen <chadallenmagic@gmail.com>
2024-05-31 11:18:38 -0700

    added a function

--- pegasus-party.js
+++ pegasus-party.js
@@ -2,11 +2,16 @@ let greeting =
   "Pretty Pegasus wants to go to the Pantheon to party with the gods. ";
 console.log(greeting);
 
-let die1 = Math.ceil(6 * Math.random());
+let die1 = rollD6(); 
 console.log(die1);
 let greeting2 = "Poor Pegasus";
 let greeting3 = "Party on Pegasus!";
-if (die1 > 1) {console.log (greeting2);
+if (die1 > 1) {
+  console.log(greeting2);
 } else {
-  console.log (greeting3);
+  console.log(greeting3);
+}
+function rollD6() {
+  let outcome = Math.ceil(6 * Math.random());
+              return outcome;
 }
