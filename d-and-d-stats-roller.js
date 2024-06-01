d6ebbd92a065781d7345e1797ba4f7b1249246fa
Chad Allen <chadallenmagic@gmail.com>
2024-06-01 14:07:19 -0700

    finished roller

--- d-and-d-stats-roller.js
+++ d-and-d-stats-roller.js
@@ -1,22 +1,37 @@
 console.log("Hello Adventurer.");
-let result1 = rollD6();
-let result2 = rollD6();
-let result3 = rollD6();
-let result4 = rollD6();
-const rolls = [result1, result2, result3, result4];
-rolls.sort();
-console.log(rolls);
+const allStats = [];
+allStats.push(rollOneStat());
+allStats.push(rollOneStat());
+allStats.push(rollOneStat());
+allStats.push(rollOneStat());
+allStats.push(rollOneStat());
+allStats.push(rollOneStat());
+allStats.push(rollOneStat());
+allStats.sort(compareNumbers);
+let garbageNumber = allStats.shift();
+console.log("discard roll", garbageNumber);
 
-rolls.shift();
-console.log(rolls);
-//calc total
-let statRollTotal = rolls[0] + rolls[1] + rolls[2];
-console.log(statRollTotal);
+console.log(allStats);
+function rollOneStat() {
+  let result1 = rollD6();
+  let result2 = rollD6();
+  let result3 = rollD6();
+  let result4 = rollD6();
+  const rolls = [result1, result2, result3, result4];
+  rolls.sort();
 
-
-
+  rolls.shift();
+  //console.log(rolls);
+  //calc total
+  let statRollTotal = rolls[0] + rolls[1] + rolls[2];
+  return statRollTotal;
+}
 
 function rollD6() {
   let outcome = Math.ceil(6 * Math.random());
   return outcome;
+}
+
+function compareNumbers(a, b) {
+  return a - b;
 }
