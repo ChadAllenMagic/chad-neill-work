09a09a8cfe6d33f066638f832cdaeb28b691abc4
Chad Allen <chadallenmagic@gmail.com>
2024-06-01 11:07:04 -0700

    objects exercises

---
+++ albums-objects.js
@@ -0,0 +1,4 @@
+const album1 = {band: "Metallica", title: "Kill 'Em All", year: 1983, numListens: 1000};
+console.log(album1.title, album1.numListens);
+album1.numListens += 5;
+console.log(album1.title, album1.numListens);
---
+++ card-objects.js
@@ -0,0 +1,4 @@
+const card1 = { suit: "spades", rank: "ace", color: "black" };
+const card2 = { suit: "hearts", rank: "ace", color: "red" };
+console.log(card2.color);
+console.log(card1.suit);
